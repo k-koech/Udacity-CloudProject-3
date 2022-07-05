67f3890edbad4b35fc3e954d0a7e2082d9f37f45
k-koech <kelvin.koech@student.moringaschool.com>
2022-07-05 08:11:19 +0300

    initial commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,25 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=kkkkkkkk
+export POSTGRES_HOST="database-1.ctnacfzkmp6s.us-east-2.rds.amazonaws.com"  
+# 127.0.0.1
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+# aws
+export AWS_BUCKET="udagram-devs"
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=testingwfa
 export URL=http://localhost:8100
+
+# export const POSTGRES_USERNAME="postgres";
+# export const POSTGRES_PASSWORD="kkkkkkkk";
+# export const POSTGRES_DB="database-1";
+# export const POSTGRES_HOST="database-1.cwfdmy2nv34b.us-west-1.rds.amazonaws.com";
+# export const dialect="postgres";
+# export const AWS_REGION="us-west-1"
+# export const aAWS_PROFILE="default";
+# export const AWS_BUCKET="udagram-dev1"
+
+export const JWT_SECRET="tyfuxbnefasv"
+
