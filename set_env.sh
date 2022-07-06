074aae44ee0acdfc6aee5113c5f7dc392056e8ea
k-koech <kelvin.koech@student.moringaschool.com>
2022-07-06 11:40:25 +0300

    Added readme and screenshots

--- set_env.sh
+++ set_env.sh
@@ -3,23 +3,14 @@
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=kkkkkkkk
 export POSTGRES_HOST="database-1.ctnacfzkmp6s.us-east-2.rds.amazonaws.com"  
-# 127.0.0.1
+
 export POSTGRES_DB=postgres
-# aws
+
 export AWS_BUCKET="udagram-devs"
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testingwfa
 export URL=http://localhost:8100
-
-# export const POSTGRES_USERNAME="postgres";
-# export const POSTGRES_PASSWORD="kkkkkkkk";
-# export const POSTGRES_DB="database-1";
-# export const POSTGRES_HOST="database-1.cwfdmy2nv34b.us-west-1.rds.amazonaws.com";
-# export const dialect="postgres";
-# export const AWS_REGION="us-west-1"
-# export const aAWS_PROFILE="default";
-# export const AWS_BUCKET="udagram-dev1"
 
 export const JWT_SECRET="tyfuxbnefasv"
 
