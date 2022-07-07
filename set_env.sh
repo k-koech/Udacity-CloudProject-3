4b8c58e1f96bb48a3883dd0ed7af19d7b18d0120
k-koech <kelvin.koech@student.moringaschool.com>
2022-07-07 07:33:34 +0300

    Added hpa screenshot

--- set_env.sh
+++ set_env.sh
@@ -17,17 +17,17 @@ export const JWT_SECRET="tyfuxbnefasv"
 export 
 "
 NAME                                      REFERENCE                          TARGETS     MINPODS     MAXPODS     REPLICAS   AGE
-udagram-api-user                      Deployment/udagram-api-user    0%/50%      1           10          1          84S
+udagram-api-user                      Deployment/udagram-api-user            0%/50%      1           10          1          70s
    k8s git:(master)xkubectl describe hpa
 
 Name:                                                                  udagram-api-user
 Namespace:                                                             default
 Labels:                                                                <none>
 Annotations:                                                           <none>
-Creation Timestamp:                                                    Wed, 6 July 2022 11:48:29 +0300
+Creation Timestamp:                                                    Thur, 7 July 2022 07:08:17 +0300
 Reference:                                                             Deployment/udagram-api-user
 Metrics:                                                               ( current / target )
-      resource cpu on pods(asapercentage of request):                  <unknown> / 50%
+      resource cpu on pods(asapercentage of request):                  0% (0) / 50%
 Min replicas:                                                          1
 Max replicas:                                                          10
 Deployment pods:                                                       1 current / 1 desired
@@ -37,9 +37,4 @@ Conditions:
   AbleToScale       True          ScaleDownStabilized    recent recommendations were higher than current one,applying the highest recent recommendation
   ScalingActive     True          ValidMetricFound       the HPA was able to successfully calculateareplica count from cpu resource utilization(pecentage of request)
   ScalingLimited    False         DesiredWithinRange     the desired count is within the acceptable range
-Events:             <none>         
-  "
-  
-
-
-
+Events:             <none>"
